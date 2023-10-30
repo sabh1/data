b1bc12d98ecee88f83d3397aa5fa288efd17f53d
sabh1 <149249934+sabh1@users.noreply.github.com>
2023-10-30 06:51:57 +0530

    zhole

--- random forest exp 4/ml4.py
+++ random forest exp 4/ml4.py
@@ -3,6 +3,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 import seaborn as sns
+from sklearn.metrics import accuracy_score, precision_score, recall_score,f1_score, confusion_matrix
 from sklearn.preprocessing import LabelEncoder
 from sklearn.ensemble import RandomForestClassifier
 
@@ -18,36 +19,39 @@ data['income'] = income_encoder.fit_transform(data['income'])
 # Perform one-hot encoding for categorical features
 categorical_cols = ["workclass", "education", "marital.status", "occupation", "relationship", "race", "sex", "native.country"]
 data = pd.get_dummies(data, columns=categorical_cols, drop_first=True)
-
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-
-# Initialize the Random Forest classifier
+# Split the dataset into features (X) and target variable (y)
+X = data.drop("income", axis=1) # Features
+y = data["income"] # Target variable
+# Split the dataset into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, 
+random_state=42)
+# heatmap
+correlation_matrix = data.corr()
+# Plot the correlation heatmap
+plt.figure(figsize=(12, 10))
+sns.heatmap(correlation_matrix, annot=True, cmap="coolwarm", fmt=".2f")
+plt.title("Correlation Heatmap")
+plt.show()
+# Random Forest Classifier
 rf_classifier = RandomForestClassifier(n_estimators=100, random_state=42)
-
-# Train the Random Forest model
+# Train 
 rf_classifier.fit(X_train, y_train)
-
-# Make predictions on the test set
+# Make predictions
 y_pred = rf_classifier.predict(X_test)
-
-# Evaluate the model
+# Performance Evaluation
 accuracy = accuracy_score(y_test, y_pred)
 precision = precision_score(y_test, y_pred)
 recall = recall_score(y_test, y_pred)
 f1 = f1_score(y_test, y_pred)
-
-# Print the evaluation metrics
-print(f"Accuracy: {accuracy:.2f}")
-print(f"Precision: {precision:.2f}")
-print(f"Recall: {recall:.2f}")
-print(f"F1 Score: {f1:.2f}")
-
-# Feature Importance
-feature_importance = pd.DataFrame({
-    'Feature': X.columns,
-    'Importance': rf_classifier.feature_importances_
-})
-
-feature_importance = feature_importance.sort_values(by='Importance', ascending=False)
-print("Feature Importance:")
-print(feature_importance)
+conf_matrix = confusion_matrix(y_test, y_pred)
+print(f"Accuracy: {accuracy}")
+print(f"Precision: {precision}")
+print(f"Recall: {recall}")
+print(f"F1 Score: {f1}")
+# confusion matrix(plot)
+plt.figure(figsize=(8, 6))
+sns.heatmap(conf_matrix, annot=True, fmt="d", cmap="Blues", cbar=False)
+plt.xlabel("Predicted Labels")
+plt.ylabel("True Labels")
+plt.title("Confusion Matrix")
+plt.show()
